ea395ecf1491853251e3582f9cfd08e3d6c1b428
Raul <nalvartediego40@gmail.com>
2022-10-31 11:15:19 -0500

    Tablas

--- src/js/main.js
+++ src/js/main.js
@@ -46,6 +46,88 @@ $(document).ready(function () {
     },
   });
 
+  
+  tableSucursal = $("#tabla_sucursals").DataTable({
+    destroy: true,
+    ajax: "mostrar-sucursal.php",
+    columns: [
+      { data: "id" },
+      { data: "id_empresa" },
+      { data: "nombre" },
+      { data: "codigo_cofide" },
+      { data: "direccion" },
+      { data: "ubigeo" },
+      {
+        defaultContent: `
+        <i class="bi bi-pencil-square btn-edit-sucursal"></i>
+        <i class="bi bi-person-plus btn-agregar-acceso"></i>
+        `,
+      },
+      {
+        defaultContent: `<i class="bi bi-x-circle-fill btn-delete-sucursal"></i>`,
+      },
+    ],
+    language: {
+      decimal: "",
+      emptyTable: "No hay información",
+      info: "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+      infoEmpty: "Mostrando 0 to 0 of 0 Entradas",
+      infoFiltered: "(Filtrado de _MAX_ total entradas)",
+      infoPostFix: "",
+      thousands: ",",
+      lengthMenu: "Mostrar _MENU_ Entradas",
+      loadingRecords: "Cargando...",
+      processing: "Procesando...",
+      search: "Sucursal",
+      zeroRecords: "Sin resultados encontrados",
+      paginate: {
+        first: "Primero",
+        last: "Ultimo",
+        next: "Siguiente",
+        previous: "Anterior",
+      },
+    },
+  });
+
+  tablaContactos = $("#tabla_contactoss").DataTable({
+    destroy: true,
+    ajax: "mostrar-contactos.php?id=1",
+    columns: [
+      { data: "id" },
+      { data: "id_empresa" },
+      { data: "nombre" },
+      { data: "cargo" },
+      { data: "telefono" },
+      { data: "correo" },
+      {
+        defaultContent: `<i class="bi bi-pencil-square btn-edit-contacto"></i>`,
+      },
+      {
+        defaultContent: `<i class="bi bi-x-circle-fill btn-delete-contacto"></i>`,
+      },
+    ],
+    language: {
+      decimal: "",
+      emptyTable: "No hay información",
+      info: "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+      infoEmpty: "Mostrando 0 to 0 of 0 Entradas",
+      infoFiltered: "(Filtrado de _MAX_ total entradas)",
+      infoPostFix: "",
+      thousands: ",",
+      lengthMenu: "Mostrar _MENU_ Entradas",
+      loadingRecords: "Cargando...",
+      processing: "Procesando...",
+      search: "Contactos ",
+      zeroRecords: "Sin resultados encontrados",
+      paginate: {
+        first: "Primero",
+        last: "Ultimo",
+        next: "Siguiente",
+        previous: "Anterior",
+      },
+    },
+  });
+
   tablaAccesos = $("#tabla_accesos").DataTable({
     destroy: true,
     ajax: {
